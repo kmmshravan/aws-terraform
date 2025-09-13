a1c33fb5fdd018380c4343be4f0068f613b7d4af
kmmshravan <kmmshravan@gmail.com>
2025-09-13 21:24:16 +0000

    with tfvars file

---
+++ ec2-variable/terraform.tfvars
@@ -0,0 +1,2 @@
+ec2ami = "ami-0cfde0ea8edd312d4"
+ec2type = "t3.micro"
--- ec2-variable/variable.tf
+++ ec2-variable/variable.tf
@@ -1,11 +1,9 @@
 variable "ec2ami" {
-  default = "ami-0cfde0ea8edd312d4"
   description = "ec2 ami"
   type = string
   
 }
 variable "ec2type" {
-  default = "t3.micro"
   description = "ec2 type"
   type = string
   
